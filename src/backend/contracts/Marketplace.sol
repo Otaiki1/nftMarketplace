5acb51fe56709c9fbf593ce37fee2a7105ca8e4b
Otaiki1 <otaikisadiq2804@gmail.com>
2022-08-18 17:38:35 +0100

    refac: Modify Marketplace contract to allow listing of items on the marketplace

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -12,10 +12,51 @@ contract Marketplace is ReentrancyGuard{
     uint public immutable feePercent;
     uint public itemCount;
 
+    //store data of the item 
+    struct Item{
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    //an event thats is triggered whenever an item is added to the marketplace
+    event Offered(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
+    
+    //mapping the itemId to the item
+    mapping(uint => Item) public items;
+
     //the immutable variables would be initialized in the constructor
     constructor(uint _feePercent){
         feeAccount = payable(msg.sender);//initializes the account collecting fees as deployer
         feePercent  = _feePercent;//sets the fee percent
+    }
+
+    function createItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant{
+        require(_price > 0, "Price Must be greater than 0");
+        //increment item count
+        itemCount++;
+        //transfer nfts
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        //add a new item to the items mapping
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
     
 
