f3b36495dc790ecbb6007eacfc0961f4898f0c63
Otaiki1 <otaikisadiq2804@gmail.com>
2022-08-19 17:57:41 +0100

    refac: Modify App.js folder to handle connection to the wallet

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,8 +1,45 @@
 
 import logo from './logo.png';
 import './App.css';
- 
+import {ethers} from "ethers";
+import {useState} from 'react';
+
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+
+import NFTAddress from "../contractsData/NFT-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+
+
+
 function App() {
+  const [account, setAccount] = useState(null);
+  const [marketplace, setMarketplace] = useState({});
+  const [NFT, setNFT] = useState({});
+  const [loading, setLoading] = useState({});
+
+  const {ethereum} = window;
+  //Metamask login and connect
+  const web3Handler = async() => {
+    const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+    setAccount(accounts[0]);
+    //get provider from metamsk
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    //set signer
+    const signer = provider.getSigner();
+    loadContracts(signer);
+  }    
+  
+  const loadContracts = async(_signer) =>{
+    const _marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, _signer);
+    setMarketplace(_marketplace);
+
+    const _nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, _signer)
+    setNFT(_nft);
+
+    setLoading(false);
+  }
+
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
