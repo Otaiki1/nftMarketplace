2ae1e2f19a98eb1a160fd9d2749f18b46bf80277
Otaiki1 <otaikisadiq2804@gmail.com>
2022-08-31 14:55:33 +0100

    refac: fix NFT contract to have mapping of address to URI List

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFT is ERC721URIStorage{
+contract NFT is ERC721URIStorage {
     //Varriable that marks token count
     uint public tokenCount;
 
-    constructor() ERC721("Dapp NFT", "DFT"){}
+    mapping(address => string[]) public nftList;
 
-//Note that the token uri serves as where the metadata is stored and the metadata in this case is link to the ipfs client
-    function mint(string memory _tokenURI) external returns(uint){
+    constructor() ERC721("Dapp NFT", "DFT") {}
+
+    //Note that the token uri serves as where the metadata is stored and the metadata in this case is link to the ipfs client
+    function mint(string memory _tokenURI) external returns (uint) {
         //we increase the total amount of NFTs first
-        tokenCount ++;
+        tokenCount++;
+        //get the index for the nftList mapping
+        uint nftCount = balanceOf(msg.sender);
         //this mints the nft and uses the token count as its ID
         _safeMint(msg.sender, tokenCount);
+        //Update the mapping
+        nftList[msg.sender][nftCount] = _tokenURI;
         //sets the token metadata to the id
         _setTokenURI(tokenCount, _tokenURI);
-        return(tokenCount);
+        return (tokenCount);
     }
-
 }
-
