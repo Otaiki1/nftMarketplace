8e59ccf416a9547b15bf243737526493c744490a
Otaiki1 <otaikisadiq2804@gmail.com>
2022-09-08 15:26:05 +0100

    refac: create function to update nft list on user purchase

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -8,7 +8,7 @@ contract NFT is ERC721URIStorage {
     uint public tokenCount;
 
     //mapping that maps each addresses to their list of ids
-    mapping(address => uint[]) private nftList;
+    mapping(address => uint[]) private _nftList;
 
     constructor() ERC721("Dapp NFT", "DFT") {}
 
@@ -19,13 +19,32 @@ contract NFT is ERC721URIStorage {
         //this mints the nft and uses the token count as its ID
         _safeMint(msg.sender, tokenCount);
         //Update the mapping
-        nftList[msg.sender].push(tokenCount);
+        _nftList[msg.sender].push(tokenCount);
         //sets the token metadata to the id
         _setTokenURI(tokenCount, _tokenURI);
         return (tokenCount);
     }
 
+    //A function that updates nft list when user buys the nft
+    // that from address would be gotten from the dapp
+
+    function updateNftList(
+        address _from,
+        uint _index,
+        uint _tokenId
+    ) external {
+        //ensure the person calling the function is the owner of the token
+        address ownerAddress = ownerOf(_tokenId);
+        require(ownerAddress == msg.sender, "Only owner can update List");
+        //delete the asset from the nft list
+        uint arrLen = _nftList[_from].length;
+        _nftList[_from][_index] = _nftList[_from][arrLen - 1];
+        _nftList[_from].pop();
+        //update the nftList for the new person
+        _nftList[msg.sender].push(_tokenId);
+    }
+
     function getNftList(address _addr) external view returns (uint[] memory) {
-        return nftList[_addr];
+        return _nftList[_addr];
     }
 }
